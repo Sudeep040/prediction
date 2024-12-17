24de19f90b36d749951b68908583a616fa88779c
Sudeep040 <109856192+Sudeep040@users.noreply.github.com>
2024-12-17 09:55:29 +0530

    Update dataProcessor.js

--- dataProcessor.js
+++ dataProcessor.js
@@ -25,13 +25,29 @@ const processCSV = (filePath) => {
                 const typeFineAgg = fineAggregateMap[row["Type_of_fine_Aggregate_"]] ?? NaN;
                 const cementOpc = parseFloat(row["Cement_O.P.C_(Kg/m3)"]);
                 const sizeCoarseAgg = parseFloat(row["Max._Size_of_Coarse_Aggregate_(mm)"]);
+                const waterContent = parseFloat(row["Water_Content_(Kg/m3)"]);
+                const wcRatio = parseFloat(row["W/C_Ratio"]);
 
                 const strength7Day = parseFloat(row["7_day_str"]);
                 const strength28Day = parseFloat(row["28_day_str"]);
 
                 // Add valid rows to the training and target datasets
-                if (!isNaN(typeCoarseAgg) && !isNaN(typeFineAgg) && !isNaN(cementOpc) && !isNaN(sizeCoarseAgg)) {
-                    trainingData.push([typeCoarseAgg, typeFineAgg, cementOpc, sizeCoarseAgg]);
+                if (
+                    !isNaN(typeCoarseAgg) &&
+                    !isNaN(typeFineAgg) &&
+                    !isNaN(cementOpc) &&
+                    !isNaN(sizeCoarseAgg) &&
+                    !isNaN(waterContent) &&
+                    !isNaN(wcRatio)
+                ) {
+                    trainingData.push([
+                        typeCoarseAgg,
+                        typeFineAgg,
+                        cementOpc,
+                        sizeCoarseAgg,
+                        waterContent,
+                        wcRatio,
+                    ]);
 
                     if (!isNaN(strength7Day)) target7Day.push(strength7Day);
                     if (!isNaN(strength28Day)) target28Day.push(strength28Day);
