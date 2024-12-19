de2fc4869171b2e2248979080526db04c02eff38
Sudeep040 <109856192+Sudeep040@users.noreply.github.com>
2024-12-19 20:51:24 +0530

    Update dataProcessor.js

--- dataProcessor.js
+++ dataProcessor.js
@@ -6,21 +6,19 @@ const processCSV = (filePath) => {
     return new Promise((resolve, reject) => {
         const trainingData = [];
         const target7Day = [];
-        const target28Day = [];
         const coarseAggregateMap = {
             Crushed: 0,
             Natural: 1,
         };
 
         const fineAggregateMap = {
-            Natural: 0,
-            Manufactured: 1,
+            Crushed: 0,
+            Natural: 1,
         };
 
         fs.createReadStream(filePath)
             .pipe(csv())
             .on("data", (row) => {
-                // Map the columns to variables
                 const typeCoarseAgg = coarseAggregateMap[row["Type_of_course_Aggregate"]] ?? NaN;
                 const typeFineAgg = fineAggregateMap[row["Type_of_fine_Aggregate_"]] ?? NaN;
                 const cementOpc = parseFloat(row["Cement_O.P.C_(Kg/m3)"]);
@@ -29,7 +27,7 @@ const processCSV = (filePath) => {
                 const wcRatio = parseFloat(row["W/C_Ratio"]);
 
                 const strength7Day = parseFloat(row["7_day_str"]);
-                const strength28Day = parseFloat(row["28_day_str"]);
+
 
                 // Add valid rows to the training and target datasets
                 if (
@@ -50,16 +48,16 @@ const processCSV = (filePath) => {
                     ]);
 
                     if (!isNaN(strength7Day)) target7Day.push(strength7Day);
-                    if (!isNaN(strength28Day)) target28Day.push(strength28Day);
                 }
             })
             .on("end", () => {
-                resolve({ trainingData, target7Day, target28Day });
+                resolve({ trainingData, target7Day });
             })
             .on("error", (error) => {
                 reject(error);
             });
     });
+
 };
 
 module.exports = { processCSV };
